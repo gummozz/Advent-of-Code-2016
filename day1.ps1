ecec8020aff6b08233b5e53b74689819d1c0be6c
gummozz <melkerssehlstedt@gmail.com>
2016-12-01 22:27:13 +0100

    Update day1.ps1

--- day1.ps1
+++ day1.ps1
@@ -1,13 +1,10 @@
-#Incomplete day2
-
-
+[string]$in = "L1, L3, L5, L3, R1, L4, L5, R1, R3, L5, R1, L3, L2, L3, R2, R2, L3, L3, R1, L2, R1, L3, L2, R4, R2, L5, R4, L5, R4, L2, R3, L2, R4, R1, L5, L4, R1, L2, R3, R1, R2, L4, R1, L2, R3, L2, L3, R5, L192, R4, L5, R4, L1, R4, L4, R2, L5, R45, L2, L5, R4, R5, L3, R5, R77, R2, R5, L5, R1, R4, L4, L4, R2, L4, L1, R191, R1, L1, L2, L2, L4, L3, R1, L3, R1, R5, R3, L1, L4, L2, L3, L1, L1, R5, L4, R1, L3, R1, L2, R1, R4, R5, L4, L2, R4, R5, L1, L2, R3, L4, R2, R2, R3, L2, L3, L5, R3, R1, L4, L3, R4, R2, R2, R2, R1, L4, R4, R1, R2, R1, L2, L2, R4, L1, L2, R3, L3, L5, L4, R4, L3, L1, L5, L3, L5, R5, L5, L4, L2, R1, L2, L4, L2, L4, L1, R4, R4, R5, R1, L4, R2, L4, L2, L4, R2, L4, L1, L2, R1, R4, R3, R2, R2, R5, L1, L2"
 $currentOrientation = "North"
 $x = 0
 $y = 0
 [array]$b = $null
 
-[string]$in = "L1, L3, L5, L3, R1, L4, L5, R1, R3, L5, R1, L3, L2, L3, R2, R2, L3, L3, R1, L2, R1, L3, L2, R4, R2, L5, R4, L5, R4, L2, R3, L2, R4, R1, L5, L4, R1, L2, R3, R1, R2, L4, R1, L2, R3, L2, L3, R5, L192, R4, L5, R4, L1, R4, L4, R2, L5, R45, L2, L5, R4, R5, L3, R5, R77, R2, R5, L5, R1, R4, L4, L4, R2, L4, L1, R191, R1, L1, L2, L2, L4, L3, R1, L3, R1, R5, R3, L1, L4, L2, L3, L1, L1, R5, L4, R1, L3, R1, L2, R1, R4, R5, L4, L2, R4, R5, L1, L2, R3, L4, R2, R2, R3, L2, L3, L5, R3, R1, L4, L3, R4, R2, R2, R2, R1, L4, R4, R1, R2, R1, L2, L2, R4, L1, L2, R3, L3, L5, L4, R4, L3, L1, L5, L3, L5, R5, L5, L4, L2, R1, L2, L4, L2, L4, L1, R4, R4, R5, R1, L4, R2, L4, L2, L4, R2, L4, L1, L2, R1, R4, R3, R2, R2, R5, L1, L2"
-#[string]$in = "R8, R4, R4, R8"
+
 $1 = $in.Split(", ")
 
 
@@ -18,7 +15,7 @@ foreach ($stuff in $1) {
     }
 
     if($stuff -match "\d+"){
-        $stepsToMove = $Matches[0]
+        [int]$stepsToMove = $Matches[0]
     }
 
     else {}
@@ -57,42 +54,96 @@ foreach ($stuff in $1) {
 
     switch ($currentOrientation) {
 
-        North {$y += $stepsToMove}
-        East {$x += $stepsToMove}
-        South {$y -= $stepsToMove}
-        West {$x -= $stepsToMove}
+        North {
+        
+
+            #$y += $stepsToMove
+
+            for ($stepsToMove -gt 0; $stepsToMove--) {
+                $y += 1
+                [array]$b += [string]$x +","+[string]$y
+
+            }
+
+
+        
+        }
+
+        East {
+        
+
+            #$x += $stepsToMove
+
+            for ($stepsToMove -gt 0; $stepsToMove--) {
+                $x += 1
+                [array]$b += [string]$x +","+[string]$y
+            }
+
+        }
+
+        South {
+        
+        
+            #$y -= $stepsToMove
+
+            for ($stepsToMove -gt 0; $stepsToMove--) {
+                $y -= 1
+                [array]$b += [string]$x +","+[string]$y
+            }
+
+        }
+
+        West {
+
+        
+            #$x -= $stepsToMove
+
+            for ($stepsToMove -gt 0; $stepsToMove--) {
+                $x -= 1
+                [array]$b += [string]$x +","+[string]$y
+            }
+
+        }
 
     }
     
-    #$stuff
-    #[string]$x +","+[string]$y
-    #$stepsToMove
-    [string]$x +","+[string]$y
-    [array]$b += [string]$x +","+[string]$y
+
 
 }
 
 
     if ($x -lt 0) {
-        "converting x to positive"
         $x = ($x * (-1))
 
     }
     if ($y -lt 0) {
-        "converting y to positive"
         $y = ($y * (-1))
 
     }
 
-    $x + $y
+    "First answer: " + ($x + $y)
 
 $a = $null
 
 
-foreach ($c in $b) {
+:outer foreach ($c in $b) {
     foreach ($d in $a) {
         if($c -eq $d) {
-            Write-Host "DING $c"
+
+        [int]$x = [int]$c.Split(",")[0]
+        [int]$y = [int]$c.Split(",")[1]
+
+
+        if ($x -lt 0) {
+        $x = ($x * (-1))
+
+        }
+        if ($y -lt 0) {
+            $y = ($y * (-1))
+
+        }
+            "Second answer: " + ($x + $y)
+            break outer
             }
     }
     $a += $c
