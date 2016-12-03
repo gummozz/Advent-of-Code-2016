417b1ed6523658bb8668c8b75df5ab20730c7587
gummozz <melkerssehlstedt@gmail.com>
2016-12-03 14:21:02 +0100

    Create 3-2.ps1

---
+++ 3-2.ps1
@@ -0,0 +1,64 @@
+#AOC 3
+[int]$answer = 0
+$i = 0
+[array]$cleanRowOne =   $null
+[array]$cleanRowTwo =   $null
+[array]$cleanRowThree = $null
+
+$instructions = "..."
+$instructionsArray = [string]$instructions -split '[\n]'
+
+foreach ($instruction in $instructionsArray) {
+    $instruction = $instruction -split '\s+'
+    if ($i -eq 3) {
+
+        if (
+
+            (([int]$cleanRowOne[0] + [int]$cleanRowOne[1]) -gt [int]$cleanRowOne[2]) -and
+            (([int]$cleanRowOne[0] + [int]$cleanRowOne[2]) -gt [int]$cleanRowOne[1]) -and
+            (([int]$cleanRowOne[1] + [int]$cleanRowOne[2]) -gt [int]$cleanRowOne[0])
+
+
+        ) {$answer++}
+
+        if (
+
+            (([int]$cleanRowTwo[0] + [int]$cleanRowTwo[1]) -gt [int]$cleanRowTwo[2]) -and
+            (([int]$cleanRowTwo[0] + [int]$cleanRowTwo[2]) -gt [int]$cleanRowTwo[1]) -and
+            (([int]$cleanRowTwo[1] + [int]$cleanRowTwo[2]) -gt [int]$cleanRowTwo[0])
+
+
+        ) {$answer++}
+
+        if (
+
+            (([int]$cleanRowThree[0] + [int]$cleanRowThree[1]) -gt [int]$cleanRowThree[2]) -and
+            (([int]$cleanRowThree[0] + [int]$cleanRowThree[2]) -gt [int]$cleanRowThree[1]) -and
+            (([int]$cleanRowThree[1] + [int]$cleanRowThree[2]) -gt [int]$cleanRowThree[0])
+
+
+        ) {$answer++}
+
+    
+
+        [array]$cleanRowOne =   $null
+        [array]$cleanRowTwo =   $null
+        [array]$cleanRowThree = $null
+        $i = 0
+
+    }
+    else {
+    
+        [array]$cleanRowOne +=   $instruction[1]
+        [array]$cleanRowTwo +=   $instruction[2]
+        [array]$cleanRowThree += $instruction[3]
+        $i++
+    
+    }
+
+
+
+}
+
+
+$answer
