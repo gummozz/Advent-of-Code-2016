b6091466a8e1f9e788f568d54af1dfcb7cf93183
gummozz <melkerssehlstedt@gmail.com>
2016-12-05 09:13:47 +0100

    Update 4-2.ps1

--- 4-2.ps1
+++ 4-2.ps1
@@ -1,42 +1,59 @@
 $instructions = "
 
 
+bkwzkqsxq-tovvilokx-nozvyiwoxd-172[fstek]
+wifilzof-wbiwifuny-yhachyylcha-526[qrazx]
+jvyyvzpcl-jhukf-shivyhavyf-487[zhtsi]
+kwvacumz-ozilm-kivlg-kwvbiqvumvb-694[gknyw]
+mvhkvbdib-kmjezxodgz-mvwwdo-omvdidib-837[dmvbi]
+nzydfxpc-rclop-qwzhpc-lnbftdtetzy-171[cptzd]
+vhehkyne-unggr-inkvatlbgz-813[gnehk]
 
-a-b-c-343[decoy]
+
+
 
 " -split '\n'
 
 
 foreach ($instruction in $instructions) {
-
     
-    $splittedInstruction = ($instruction -split '[\[]')[0]
-    #$ID = ($splittedInstruction.Split('-')[(($splittedInstruction.Split('-')).length) -1])
-    [int]$ID = ($splittedInstruction.Split('-')[3])
-    $ID
-    if($splittedInstruction) {
-    $splittedInstructionWithoutID = $splittedInstruction.Replace($ID,'').replace('-',' ')
-    }
+    
 
-    $charArray = $null
+    if ($instruction -match '\[(.+?)\]') {
+    
+        $splittedInstruction = ($instruction -split '[\[]')[0]
+        [int]$ID = ($splittedInstruction.Split('-')[(($splittedInstruction.Split('-')).length) -1])
+        if($splittedInstruction) {
+        $splittedInstructionWithoutID = $splittedInstruction.Replace($ID,'').replace('-',' ')
+        }
 
-    $splittedInstructionWithoutID.ToCharArray() | % {$charArray += [byte][char]$_}
-    #$ID
+        $charArray = $null
+        $splittedInstructionWithoutID.ToCharArray() | % {[array]$charArray += [byte][char]$_}
+        #$ID
+        [string]$currentSentence = [string]$ID + "-"
+        foreach ($char in $charArray) {
+            #[string]$currentSentence
+            [int]$tempChar = $char
 
-    foreach ($char in $charArray) {
 
-        [int]$tempChar = $char
+            for ($i = $ID; $i -gt 0; $i--) {
+                if ($tempChar -ne 32){
+                    if ($tempChar -eq 122) {$tempChar = 97}
+                    else {$tempChar++}
+                }
 
 
-        for ($i = $ID; $i -gt 0; $i--) {
+            }
+            #[char][int]$tempChar
+           [string]$currentSentence += [char][int]$tempChar
 
-            if ($tempChar -eq 122) {$tempChar = 99}
-            else {$tempChar++}
 
         }
-        [char][int]$tempChar
 
-
+        if ($currentSentence -match "North|Pole|Objects"){
+        $currentSentence
+        }
     }
 
+    
 }
