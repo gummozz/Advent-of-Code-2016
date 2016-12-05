a306a2d951e87b81106c9b922dc92a2e11772d4a
gummozz <melkerssehlstedt@gmail.com>
2016-12-05 15:11:04 +0100

    Update 4-1.ps1

--- 4-1.ps1
+++ 4-1.ps1
@@ -27,13 +27,21 @@ foreach ($instruction in $instructions) {
     $instructionWithoutDashesOrNumbers = ($instructionWithoutDashesOrNumbers -split '[\[]' )[0]
     $temp1 = $null
     $temp2 = $null
+    $temp3 = $null
+    $temp4 = $null
     $improvedInstruction = $null
     $instructionWithoutDashesOrNumbers.tochararray() | foreach {if ($checksum -match $_){$improvedInstruction += $_}}
+
+
+    #UGLY FIX
+    $instructionWithoutDashesOrNumbers.tochararray() | Sort-Object | Group-Object | Sort-Object -Property "Count" -Descending | % {[string]$temp3 += $_.name}
+    $temp3.ToCharArray() | Select-Object -First 1 | % {$temp4 += $_}
+    #UGLY FIX
 
     if ($improvedInstruction.Length -gt 4) {
     $improvedInstruction.tochararray() | Sort-Object | Group-Object | Sort-Object -Property "Count" -Descending | % {[string]$temp1 += $_.name}
     $temp1.ToCharArray() | Select-Object -First 5 | % {$temp2 += $_}
-    if($temp2 -eq $checkSum) {$answer += $numberAtTheEnd
+    if($temp2 -eq $checkSum -and $temp2 -match $temp4) {$answer += $numberAtTheEnd
     Write-Host -ForegroundColor Green "The calculated checksum of $instruction is $temp2 and should be $checksum"
     }
     else {
